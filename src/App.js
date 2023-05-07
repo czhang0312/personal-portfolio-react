30faa6b7015acf6958305245c061bceb357c7320
clarkzhang <czhang0312>
2023-05-06 21:15:04 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,94 +1,89 @@
 import React from "react";
-import Navbar from './Components/Navbar'
-
+import Navbar from "./Components/Navbar"
+import Section from "./Components/Section"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
-class Section extends React.Component {
-    render() {
-        return (
-            <section>
-                <h2>
-                    {this.props.name}
-                </h2>
-                <div className='section-content'>
-                    {this.props.content}
-                </div>
-            </section>
-        )
-    }
-}
 
-function App() {
+export default function App() {
     return (
         <div>
             <Navbar />
-            <Section name="About Me" content={aboutMe} />
-            <Section name="Projects" content={projects} />
-            <Section name="Fun" content={fun} />
-            <Section name="Contact" content={contact} />
+            <Section name="About Me" content={<AboutMe />} />
+            <Section name="Projects" content={<Projects />} />
+            <Section name="Fun" content={<Fun />} />
+            <Section name="Contact" content={<Contact />} />
             <br></br>
         </div>
     );
-}
-
-export default App
-
-// content stored in const variables
-
-const aboutMe = <div>
-    <img src="headshot.png" alt="profile" class="me" />
-    <p> Hello! I'm an undergraduate student studying computer science at the University of Toronto. Currently, I am working on some front-end projects such as this one,
-        but I hope to become a full-stack developer someday!</p>
-
-</div>
+};
 
 
-
-const projects = <section class="project-section" id="projects">
-    <article>
-        <a href="https://github.com/CSC207-2022F-UofT/course-project-group-97">
-            <img src="datingApp.png" alt="dating app" id="dating" />
-        </a>
-        <div>
-            <h3>Dating App</h3>
-            <p> I worked on this project for my software design course. With my team we designed a full-stack application completely
-                using Java following good software development practices. The feature I worked on specifically was the chat room functionality,
-                which allowed users to communicate to other users they matched with.
-            </p>
-        </div>
-    </article>
-
-    <article>
-        <a href="https://github.com/czhang0312/personal-portfolio-react">
-            <img src="website.png" alt="my website" id="website" />
-        </a>
-        <div>
-            <h3>Personal Website</h3>
-            <p>I first created this project from scratch using bare HTML/CSS Javascript. But recently I
-                migrated my website to React, which you are viewing now.
-            </p>
-        </div>
-    </article>
-
-    <article>
-        <a href="https://github.com/czhang0312/TicTacToe">
-            <img src="tictactoe.png" alt="tictactoe" id="tictactoe" />
-        </a>
-        <div>
-            <h3>Tic Tac Toe</h3>
-            <p>This project is my first front-end project I built using only HTML, CSS and JS. I used CSS tables to create the game board and coded the game mechanics
-                using Javascript. I learned a lot about how HTML, CSS and JS interact with each other to create the game.
-            </p>
-        </div>
-    </article>
-</section>
+function AboutMe() {
+    return (
+      <div>
+        <img src="headshot.png" alt="profile" className="me" />
+        <p>
+          Hello! I'm an undergraduate student studying computer science at the University of Toronto. Currently, I am working on some front-end projects such as this one, but I hope to become a full-stack developer someday!
+        </p>
+      </div>
+    );
+  };
 
 
+function Projects() {
+    return (<section class="project-section" id="projects">
+            <article>
+                <a href="https://github.com/CSC207-2022F-UofT/course-project-group-97">
+                    <img src="datingApp.png" alt="dating app" id="dating" />
+                </a>
+                <div>
+                    <h3>Dating App</h3>
+                    <p> I worked on this project for my software design course. With my team we designed a full-stack application completely
+                        using Java following good software development practices. The feature I worked on specifically was the chat room functionality,
+                        which allowed users to communicate to other users they matched with.
+                    </p>
+                </div>
+            </article>
 
-const fun = <p> I enjoy working out, snowboarding, and listening to music! </p>
+            <article>
+                <a href="https://github.com/czhang0312/personal-portfolio-react">
+                    <img src="website.png" alt="my website" id="website" />
+                </a>
+                <div>
+                    <h3>Personal Website</h3>
+                    <p>I first created this project from scratch using bare HTML/CSS Javascript. But recently I
+                        migrated my website to React, which you are viewing now.
+                    </p>
+                </div>
+            </article>
 
-const contact = <section class="contact-section" id="contact">
+            <article>
+                <a href="https://github.com/czhang0312/TicTacToe">
+                    <img src="tictactoe.png" alt="tictactoe" id="tictactoe" />
+                </a>
+                <div>
+                    <h3>Tic Tac Toe</h3>
+                    <p>This project is my first front-end project I built using only HTML, CSS and JS. I used CSS tables to create the game board and coded the game mechanics
+                        using Javascript. I learned a lot about how HTML, CSS and JS interact with each other to create the game.
+                    </p>
+                </div>
+            </article>
+            </section>
+    );
+  };
+
+
+function Fun() {
+    return (
+        <p id="fun"> I enjoy working out, snowboarding, and listening to music! </p>
+    );
+    
+};
+
+
+function Contact() {
+    return (<section class="contact-section" id="contact">
     <p> Reach me at clarkw.zhang@mail.utoronto.ca</p>
     <div class="icon-container">
         <a href="https://github.com/czhang0312" class="icon">
@@ -101,5 +96,7 @@ const contact = <section class="contact-section" id="contact">
             <FontAwesomeIcon icon={faInstagram} />
         </a>
     </div>
-</section>
+    </section>
+    );
+};
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 export default function createNavbar() {
-    return (<nav class="navbar">
-        <div class="logo">Clark Zhang</div>
-        <a class="toggle-button">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </a>
-        <div class="navbar-links">
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#fun">Fun</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
-    </nav>);
-}
+    return (<nav className="navbar">
+                <div className="logo">Clark Zhang</div>
+                <a href="#" className="toggle-button">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </a>
+                <div className="navbar-links">
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#fun">Fun</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>);
+};
